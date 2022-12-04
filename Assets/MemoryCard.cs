6ccbcae5961cd6a9af9cac02b754cb7bfa664736
Nikita Davydov <nikita.davidov02@yandex.ru>
2022-12-05 02:16:50 +0300

    Initial commit

---
+++ Assets/MemoryCard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cardBack;
+    [SerializeField]
+    private SceneController controller;
+
+    private int _id;
+    public int id { get { return _id; } }
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf && controller.canRevealed)
+        {
+            cardBack.SetActive(false);
+            controller.CanRevealed(this);
+        }
+    }
+    public void Unrevealed()
+    {
+        cardBack.SetActive(true);
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
